6a796c7cc3549ee4dbfd8c1748a8bbf8ba38b2d3
ramenjuniti <ramenjuniti@gmail.com>
2020-08-01 16:47:53 +0900

    peer

--- index.js
+++ index.js
@@ -9,4 +9,31 @@
     } catch (error) {
         alert(error)
     }
+
+    const peer = new Peer({
+        key: '3e098f7d-5efa-4b77-b45e-1f817b46755f',
+        debug: 3
+    })
+
+    peer.on('open', () => {
+        document.getElementById('my-id').textContent = peer.id
+    })
+
+    document.getElementById('make-call').onclick = () => {
+        const theirID = document.getElementById('their-id').value;
+        const mediaConnection = peer.call(theirID, localStream);
+        setEventListener(mediaConnection)
+    }
+
+    const setEventListener = mediaConnection => {
+        mediaConnection.on('stream', stream => {
+            const videoElement = document.getElementById('their-video');
+            videoElement.srcObject = stream;
+            videoElement.Play();
+        })
+    }
+
+    peer.on('open', () => {
+        document.getElementById('my-id').textContent = peer.id;
+    });
 })();
