eb4910bbe08830e1c9bdd5b64d1f89cb94c3dfc9
ramenjuniti <ramenjuniti@gmail.com>
2020-08-01 16:17:41 +0900

    camera写った

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-(function(){
+(async function () {
     console.log("hello treasure");
+    let localStream;
+    try {
+        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        const videoElement = document.getElementById('my-video');
+        videoElement.srcObject = localStream;
+        videoElement.play()
+    } catch (error) {
+        alert(error)
+    }
 })();
